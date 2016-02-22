592058c44dc49f1a5b3ac0c20e571bf2a03a2a9e
Vladislav Rassokhin <vladrassokhin@gmail.com>
2016-02-22 03:01:33 +0300

    Improvements in highlighter
    Use TokenSet instead of java.lang.Set, as it uses special internal structure and works faster;
    Highlight 'in', 'all' as keywords;
    Use prefix for text attributes, it's important to not to redefine standard text attributes;
    Highlight arithmetic operators, brackets, braces ,parentheses, dots and colons.

--- IDEA/src/ksp/kos/ideaplugin/KerboScriptSyntaxHighlighter.java
+++ IDEA/src/ksp/kos/ideaplugin/KerboScriptSyntaxHighlighter.java
@@ -5,14 +5,11 @@ import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import ksp.kos.ideaplugin.parser.KerboScriptLexerAdapter;
 import ksp.kos.ideaplugin.parser.KerboScriptParserDefinition;
 import ksp.kos.ideaplugin.psi.KerboScriptTypes;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created on 27/12/15.
@@ -20,14 +17,22 @@ import java.util.Set;
  * @author ptasha
  */
 public class KerboScriptSyntaxHighlighter extends SyntaxHighlighterBase {
-    public static final TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey("KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
-    public static final TextAttributesKey IDENTIFIER = TextAttributesKey.createTextAttributesKey("IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER);
-    public static final TextAttributesKey COMMENT = TextAttributesKey.createTextAttributesKey("COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey STRING = TextAttributesKey.createTextAttributesKey("STRING", DefaultLanguageHighlighterColors.STRING);
-    public static final TextAttributesKey NUMBER = TextAttributesKey.createTextAttributesKey("NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    private static final String PREFIX = "KOS.";
+    public static final TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey(PREFIX + "KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey IDENTIFIER = TextAttributesKey.createTextAttributesKey(PREFIX + "IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER);
+    public static final TextAttributesKey COMMENT = TextAttributesKey.createTextAttributesKey(PREFIX + "COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey STRING = TextAttributesKey.createTextAttributesKey(PREFIX + "STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey NUMBER = TextAttributesKey.createTextAttributesKey(PREFIX + "NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    public static final TextAttributesKey OPERATORS_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "OPERATOR_SIGN", DefaultLanguageHighlighterColors.OPERATION_SIGN);
+    public static final TextAttributesKey PARENTHESES_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey BRACES_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "BRACES", DefaultLanguageHighlighterColors.BRACES);
+    public static final TextAttributesKey BRACKETS_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "BRACKETS", DefaultLanguageHighlighterColors.BRACKETS);
 
-    public static final Set<IElementType> KEYWORDS = new HashSet<>(
-            Arrays.asList(
+    // It's intended since KOS using '.' as end of instruction and ':' as member.field separator.
+    public static final TextAttributesKey EOI_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "DOT", DefaultLanguageHighlighterColors.SEMICOLON);
+    public static final TextAttributesKey COLON_KEY = TextAttributesKey.createTextAttributesKey(PREFIX + "COLON", DefaultLanguageHighlighterColors.DOT);
+
+    public static final TokenSet KEYWORDS = TokenSet.create(
                     KerboScriptTypes.NOT,
                     KerboScriptTypes.AND,
                     KerboScriptTypes.OR,
@@ -80,10 +85,40 @@ public class KerboScriptSyntaxHighlighter extends SyntaxHighlighterBase {
                     KerboScriptTypes.SHUTDOWN,
                     KerboScriptTypes.FOR,
                     KerboScriptTypes.UNSET,
+                    KerboScriptTypes.IN,
+                    KerboScriptTypes.ALL,
                     KerboScriptTypes.ATSIGN,
                     KerboScriptTypes.LAZYGLOBAL
-            )
     );
+
+    public static final TokenSet OPERATORS = TokenSet.create(
+            KerboScriptTypes.COMPARATOR,
+            KerboScriptTypes.PLUSMINUS,
+            KerboScriptTypes.MULT,
+            KerboScriptTypes.DIV,
+            KerboScriptTypes.POWER
+    );
+    public static final TokenSet NUMBERS = TokenSet.create(
+            KerboScriptTypes.E,
+            KerboScriptTypes.DOUBLE,
+            KerboScriptTypes.INTEGER
+    );
+
+    // Proper names described here: https://www.cis.upenn.edu/~matuszek/General/JavaSyntax/parentheses.html
+    // And here: https://en.wikipedia.org/wiki/Bracket
+    public static final TokenSet PARENTHESES = TokenSet.create(
+            KerboScriptTypes.BRACKETOPEN,
+            KerboScriptTypes.BRACKETCLOSE
+    );
+    public static final TokenSet BRACES = TokenSet.create(
+            KerboScriptTypes.CURLYOPEN,
+            KerboScriptTypes.CURLYCLOSE
+    );
+    public static final TokenSet BRACKETS = TokenSet.create(
+            KerboScriptTypes.SQUAREOPEN,
+            KerboScriptTypes.SQUARECLOSE
+    );
+
 
     @NotNull
     @Override
@@ -94,22 +129,30 @@ public class KerboScriptSyntaxHighlighter extends SyntaxHighlighterBase {
     @NotNull
     @Override
     public TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
-        if (tokenType.equals(KerboScriptTypes.IDENTIFIER)) {
-            return createKeys(IDENTIFIER);
+        if (KerboScriptTypes.IDENTIFIER.equals(tokenType)) {
+            return pack(IDENTIFIER);
         } else if (KerboScriptParserDefinition.COMMENTS.contains(tokenType)) {
-            return createKeys(COMMENT);
-        } else if (tokenType.equals(KerboScriptTypes.STRING)) {
-            return createKeys(STRING);
+            return pack(COMMENT);
+        } else if (KerboScriptTypes.STRING.equals(tokenType)) {
+            return pack(STRING);
+        } else if (KerboScriptTypes.EOI.equals(tokenType)) {
+            return pack(EOI_KEY);
+        } else if (KerboScriptTypes.COLON.equals(tokenType)) {
+            return pack(COLON_KEY);
         } else if (KEYWORDS.contains(tokenType)) {
-            return createKeys(KEYWORD);
-        } else if (tokenType.equals(KerboScriptTypes.E) || tokenType.equals(KerboScriptTypes.INTEGER) || tokenType.equals(KerboScriptTypes.DOUBLE)) {
-            return createKeys(NUMBER);
+            return pack(KEYWORD);
+        } else if (NUMBERS.contains(tokenType)) {
+            return pack(NUMBER);
+        } else if (OPERATORS.contains(tokenType)) {
+            return pack(OPERATORS_KEY);
+        } else if (PARENTHESES.contains(tokenType)) {
+            return pack(PARENTHESES_KEY);
+        } else if (BRACES.contains(tokenType)) {
+            return pack(BRACES_KEY);
+        } else if (BRACKETS.contains(tokenType)) {
+            return pack(BRACKETS_KEY);
         }
         return new TextAttributesKey[0];
     }
 
-    @NotNull
-    private TextAttributesKey[] createKeys(TextAttributesKey identifier) {
-        return new TextAttributesKey[]{identifier};
-    }
 }
