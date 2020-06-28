f233abeaa8302bf449e76f5246b66b4732cc7cab
Abby Berkers <abbyberkers@gmail.com>
2020-06-28 19:28:20 +0200

    [IDEA] Add new file dialog

---
+++ IDEA/src/ksp/kos/ideaplugin/actions/NewKerboScriptFileAction.kt
@@ -0,0 +1,47 @@
+package ksp.kos.ideaplugin.actions
+
+import com.intellij.icons.AllIcons
+import com.intellij.ide.actions.CreateElementActionBase
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.util.Consumer
+import ksp.kos.ideaplugin.KerboScriptFileType
+
+class NewKerboScriptFileAction : CreateElementActionBase("KerboScript File", "Create new KerboScript file", AllIcons.FileTypes.Text) {
+    override fun create(s: String, psiDirectory: PsiDirectory): Array<PsiElement> = arrayOf()
+
+    override fun getActionName(p0: PsiDirectory?, p1: String?): String = ""
+
+    override fun getErrorTitle(): String = "Error"
+
+    override fun invokeDialog(project: Project, directory: PsiDirectory, elementsConsumer: java.util.function.Consumer<Array<PsiElement>>) {
+        val builder = CreateFileFromTemplateDialog.createDialog(project)
+        builder.apply {
+            setTitle("Create a new KerboScript file")
+            addKind("KerboScript", AllIcons.FileTypes.Text, "ks")
+        }
+        val consumer: Consumer<in PsiElement?> = Consumer {  }
+        builder.show("", null, KerboScriptFileCreator(project, directory), consumer)
+    }
+
+    inner class KerboScriptFileCreator(val project: Project, val directory: PsiDirectory) : CreateFileFromTemplateDialog.FileCreator<PsiElement?> {
+        override fun createFile(fileName: String, option: String): PsiElement? {
+            val fileType = KerboScriptFileType.INSTANCE
+            val fileFactory = PsiFileFactory.getInstance(project)
+            val file = fileFactory.createFileFromText("$fileName.${fileType.defaultExtension}", fileType, "")
+            val psiFile = directory.add(file) as PsiFile
+            // Open the file we just created.
+            FileEditorManager.getInstance(project).openFile(psiFile.virtualFile, true)
+            return psiFile
+        }
+
+        override fun startInWriteAction(): Boolean = true
+
+        override fun getActionName(p0: String, p1: String): String = "New KerboScript file"
+    }
+}
