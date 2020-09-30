dd463ae4db1dc885c640e31f2a674a4d497d6c73
Abby Berkers <abbyberkers@gmail.com>
2020-09-30 12:26:56 +0200

    Undo redundant change

--- IDEA/src/main/java/ksp/kos/ideaplugin/completion/KerboScriptCompletionContributor.kt
+++ IDEA/src/main/java/ksp/kos/ideaplugin/completion/KerboScriptCompletionContributor.kt
@@ -3,7 +3,6 @@ package ksp.kos.ideaplugin.completion
 import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.psi.PsiElement
 import ksp.kos.ideaplugin.KerboScriptLanguage
 
 class KerboScriptCompletionContributor : CompletionContributor() {
@@ -13,10 +12,5 @@ class KerboScriptCompletionContributor : CompletionContributor() {
                 PlatformPatterns.psiElement().withLanguage(KerboScriptLanguage.INSTANCE),
                 KerboScriptKeywordProvider()
         )
-    }
-
-    override fun invokeAutoPopup(position: PsiElement, typeChar: Char): Boolean {
-        // Disable autocompletion after the . that ends a statement.
-        return typeChar != '.'
     }
 }
--- IDEA/src/main/java/ksp/kos/ideaplugin/completion/KerboScriptTypedHandler.kt
+++ IDEA/src/main/java/ksp/kos/ideaplugin/completion/KerboScriptTypedHandler.kt
@@ -3,6 +3,7 @@ package ksp.kos.ideaplugin.completion
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import ksp.kos.ideaplugin.KerboScriptFileType
 
