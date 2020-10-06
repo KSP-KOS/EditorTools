7d526ef71c28832f08b98290a1c09a45ee4c82c0
Abby Berkers <abbyberkers@gmail.com>
2020-10-06 07:52:04 +0200

    Cleanup gradle

--- IDEA/build.gradle.kts
+++ IDEA/build.gradle.kts
@@ -44,9 +44,6 @@ dependencies {
     // Use junit 5.
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-
-//    testImplementation("org.junit.platform:junit-platform-runner:1.7.0")
-//    testImplementation("org.junit.platform:junit-platform-commons:1.7.0")
 }
 
 intellij {
